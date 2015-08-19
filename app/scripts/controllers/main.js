b5e7000bc9ecbc0c73608762f7fa760f2e099ca8
RangeAge <a24887181@gmail.com>
2015-08-19 14:00:44 +0800

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fingerNodejsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fingerNodejsApp
+ */
+angular.module('fingerNodejsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
